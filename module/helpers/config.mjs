6c92a4929bc0061870d34a90da491ffe6152f37a
dragonkie <dragonkie@hotmail.ca>
2023-05-23 16:26:29 -0400

    Inital Commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,25 @@
+export const TALESFROMMYRIAD = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ TALESFROMMYRIAD.abilities = {
+  "pwr": "TALESFROMMYRIAD.AbilityPwr",
+  "fin": "TALESFROMMYRIAD.AbilityFin",
+  "ins": "TALESFROMMYRIAD.AbilityIns",
+  "chr": "TALESFROMMYRIAD.AbilityChr",
+  "arc": "TALESFROMMYRIAD.AbilityArc",
+  "occ": "TALESFROMMYRIAD.AbilityOcc",
+  "lck": "TALESFROMMYRIAD.AbilityLck"
+};
+
+TALESFROMMYRIAD.abilityAbbreviations = {
+  "pwr": "TALESFROMMYRIAD.AbilityPwrAbbr",
+  "fin": "TALESFROMMYRIAD.AbilityFinAbbr",
+  "ins": "TALESFROMMYRIAD.AbilityInsAbbr",
+  "chr": "TALESFROMMYRIAD.AbilityChrAbbr",
+  "arc": "TALESFROMMYRIAD.AbilityArcAbbr",
+  "occ": "TALESFROMMYRIAD.AbilityOccAbbr",
+  "lck": "TALESFROMMYRIAD.AbilityLckAbbr"
+};
