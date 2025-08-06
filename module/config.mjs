40e471912b1db52eb6f08d14208cc43c9c11b2b6
dragonkie <dragonkie@hotmail.ca>
2025-08-05 23:48:33 -0400

    Added NPC item displays

--- module/config.mjs
+++ module/config.mjs
@@ -230,6 +230,7 @@ TFM.DamageClass = {
 TFM.DamageTypes = {
     sharp: 'TFM.Damage.Type.Sharp',
     blunt: 'TFM.Damage.Type.Blunt',
+    corruption: 'TFM.Damage.Type.Corruption',
     cold: 'TFM.Damage.Type.Cold',
     fire: 'TFM.Damage.Type.Fire',
     sonic: 'TFM.Damage.Type.Sonic',
