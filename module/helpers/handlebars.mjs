db85c1171556a942e2265e796820d493c654f0c1
dragonkie <dragonkie@hotmail.ca>
2026-03-06 13:23:53 -0500

    Updated helpers

--- module/helpers/handlebars.mjs
+++ module/helpers/handlebars.mjs
@@ -33,50 +33,61 @@ function registerTemplates() {
 };
 
 function registerHelpers() {
-    const helpers = [
+    const helpers = {
+        //=================================================================================================
+        //>  Data Management
+        //=================================================================================================
+        JSON: (str) => JSON.parse(str),
+        isArray: (arr) => Array.isArray(arr),
+        arrayLength: (arr) => {
+            if (Array.isArray(arr)) return arr.length;
+            else throw new Error("Can't get length of non array value");
+        },
+        objectIsEmpty: (obj) => Object.keys(obj).length <= 0,
+        objectProp: (obj, property) => obj[property],
         //======================================================================================
         //> Strings
         //======================================================================================
-        { name: 'toLowerCase', fn: (str) => str.toLowerCase() },
-        { name: 'toTitleCase', fn: (str) => str.replace(/\w\S*/g, text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()) },
-
+        toLowerCase: (str) => str.toLowerCase(),
+        toTitleCase: (str) => str.replace(/\w\S*/g, text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()),
+        selected: (val) => {
+            if (val) return new Handlebars.SafeString('selected');
+            return '';
+        },
         //======================================================================================
         //> Logic
         //======================================================================================
-        { name: 'choose', fn: (a, b) => a ? a : b },
-        {
-            name: 'isEmpty', fn: (obj) => {
-                if (Array.isArray(obj)) return obj.length == 0;
-                if (typeof obj == 'object') return Object.keys(obj).length == 0;
-            }
+        choose: (a, b) => a ? a : b,
+        isEmpty: (obj) => {
+            if (Array.isArray(obj)) return obj.length == 0;
+            if (typeof obj == 'object') return Object.keys(obj).length == 0;
         },
-
         //======================================================================================
         //> User permissions
         //======================================================================================
-        { name: 'isGM', fn: () => game.user.isGM },
-
+        isGM: () => game.user.isGM,
+        gameSetting: (scope, id) => { return game.settings.get(scope, id) },
+        systemSetting: (id) => { return game.settings.get(game.system.id, id) },
         //======================================================================================
         //> Math helpers
         //======================================================================================
-        { name: 'addition', fn: (a, b) => a + b },
-        { name: 'ceil', fn: (a) => Math.ceil(a) },
-        { name: 'divide', fn: (a, b) => a / b },
-        { name: 'floor', fn: (a) => Math.floor(a) },
-        { name: 'max', fn: (...num) => Math.max(...num) },
-        { name: 'min', fn: (...num) => Math.min(...num) },
-        { name: 'multiply', fn: (a, b) => a * b },
-        { name: 'percent', fn: (a, b) => a / b * 100 },
-        { name: 'round', fn: (a) => Math.ceil(a) },
-        { name: 'subtraction', fn: (a, b) => a - b },
+        addition: (a, b) => a + b,
+        ceil: (a) => Math.ceil(a),
+        divide: (a, b) => a / b,
+        floor: (a) => Math.floor(a),
+        max: (...num) => Math.max(...num),
+        min: (...num) => Math.min(...num),
+        multiply: (a, b) => a * b,
+        percent: (a, b) => a / b * 100,
+        round: (a) => Math.ceil(a),
+        subtraction: (a, b) => a - b,
         //======================================================================================
         //> Elements
         //======================================================================================
-        {// wraps a set of elements in a collapsible wrapper
-            name: 'collapsible',
-            fn: (label, options) => {
-                if (!options) options = label, label = '';
-                return new Handlebars.SafeString(`
+        // wraps a set of elements in a collapsible wrapper
+        collapsible: (label, options) => {
+            if (!options) options = label, label = '';
+            return new Handlebars.SafeString(`
                     <div class="collapsible">
                         <div class="flexrow">
                             <a data-action="collapse"><i class="fas fa-caret-down"></i></a>
@@ -88,14 +99,11 @@ function registerHelpers() {
                             </div>
                         </div>
                     </div>`
-                );
-            }
+            );
         },
-        {
-            name: 'collapsed',
-            fn: (label, options) => {
-                if (!options) options = label, label = '';
-                return new Handlebars.SafeString(`
+        collapsed: (label, options) => {
+            if (!options) options = label, label = '';
+            return new Handlebars.SafeString(`
                     <div class="collapsible collapsed">
                         <div class="flexrow">
                             <a data-action="collapse"><i class="fas fa-caret-down"></i></a>
@@ -107,67 +115,87 @@ function registerHelpers() {
                             </div>
                         </div>
                     </div>`
-                );
-            }
+            );
         },
         //======================================================================================
         //> Iterators
         //======================================================================================
-        {
-            name: 'repeat',
-            fn: (num, options) => {
-                if (isNaN(num)) return options.fn(this);
-                for (var i = 0, ret = ''; i < num; i++) ret += options.fn(i);
-                return ret;
-            }
+        repeat: (num, options) => {
+            if (isNaN(num)) return options.fn(this);
+            for (var i = 0, ret = ''; i < num; i++) ret += options.fn(i);
+            return ret;
         },
-        //======================================================================================
-        //> Data Fields
-        //======================================================================================
-        {
-            name: 'getField',
-            fn: (schema, path) => schema.getField(path)
-        }, {
-            name: 'toFieldGroup',
-            fn: (schema, path, options) => {
-                const field = schema.getField(path);
-                if (!field) throw new Error(`Couldnt find field from [${path}] in schema:`, schema);
-
-                const { classes, label, hint, rootId, stacked, units, widget, ...inputConfig } = options.hash;
-                const groupConfig = {
-                    label, hint, rootId, stacked, widget, localize: true, units,
-                    classes: typeof classes === "string" ? classes.split(" ") : []
-                };
-
-                const group = field.toFormGroup(groupConfig, inputConfig);
-                return new Handlebars.SafeString(group.outerHTML);
+        //=================================================================================================
+        //>  Data Fields                              
+        //=================================================================================================
+        getField(schema, path) { return schema.getField(path) },
+        toFieldGroup(schema, path, options) {
+            let field = schema.getField(path);
+            const { classes, label, hint, rootId, stacked, units, widget, ...inputConfig } = options.hash;
+            const groupConfig = {
+                label, hint, rootId, stacked, widget, localize: true, units,
+                classes: typeof classes === "string" ? classes.split(" ") : []
             }
-        }, {
-            name: 'toFieldInput',
-            fn: (schema, path, options) => {
-                const field = schema.getField(path);
-                if (!field) throw new Error(`Couldnt find field from [${path}] in schema:`, schema);
-
-
-                const { classes, label, hint, rootId, stacked, units, widget, ...inputConfig } = options.hash;
-                const groupConfig = {
-                    label, hint, rootId, stacked, widget, localize: true, units,
-                    classes: typeof classes === "string" ? classes.split(" ") : []
-                };
-
-                const group = field.toInput(groupConfig, inputConfig);
-                return new Handlebars.SafeString(group.outerHTML);
+            const group = field.toFormGroup(groupConfig, inputConfig);
+            return new Handlebars.SafeString(group.outerHTML);
+        },
+        toFieldInput(schema, path, options) {
+            let field = schema.getField(path);
+            const { classes, label, hint, rootId, stacked, units, widget, ...inputConfig } = options.hash;
+            const groupConfig = {
+                label, hint, rootId, stacked, widget, localize: true, units,
+                classes: typeof classes === "string" ? classes.split(" ") : []
             }
-        }, {
-            name: 'ledger',
-            fn: (target, id, label) => {
-                return `<a data-action="editLedger" data-target="${target}" data-id="${id}" data-label="${label}"><i class="fa-solid fa-memo-pad"></i></a>`
-            }
-        }
-    ]
+            const group = field.toInput(inputConfig);
+            return new Handlebars.SafeString(group.outerHTML);
+        },
+        systemFieldInput(system, path, options) {
+            // auto fills in the details to the input using the provided path from the system data
+            // the system needs to define a schema path as well
+            const field = system?.schema.getField(path);
+            if (!system.schema) throw new Error("Missing system schema to reference");
+            if (!field) throw new Error("Couldn't find field path for system schema, was it included?" + `${path}`);
+
+            const { classes, label, hint, rootId, stacked, units, widget, ...inputConfig } = options.hash;
+            const input = path.split(".");
+            var value = system;
+            input.forEach(p => {
+                value = value[p];
+            });
+
+            inputConfig.value = value;
+
+            const group = field.toInput(inputConfig);
+            return new Handlebars.SafeString(group.outerHTML);
+        },
+        systemFieldGroup(system, path, options) {
+            // auto fills in the details to the input using the provided path from the system data
+            // the system needs to define a schema path as well
+            const field = system?.schema.getField(path);
+            if (!system.schema) throw new Error("Missing system schema to reference");
+            if (!field) throw new Error("Couldn't find field path for system schema, was it included?" + `${path}`);
+
+            const { classes, label, hint, rootId, stacked, units, widget, ...inputConfig } = options.hash;
+            const groupConfig = {
+                label, hint, rootId, stacked, widget, localize: true, units,
+                classes: typeof classes === "string" ? classes.split(" ") : []
+            };
+
+            const input = path.split(".");
+            var value = system;
+            input.forEach(p => {
+                value = value[p];
+            });
+
+            inputConfig.value = value;
+
+            const group = field.toFormGroup(groupConfig, inputConfig);
+            return new Handlebars.SafeString(group.outerHTML);
+        },
+    }
 
     // register the helpers
-    for (const helper of helpers) Handlebars.registerHelper(helper.name, helper.fn);
+    for (const [key, fn] of Object.entries(helpers)) Handlebars.registerHelper(key, fn);
 }
 
 /**
