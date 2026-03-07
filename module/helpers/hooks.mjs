83a94a50383518ec130ab8d5d642cfd887e5343f
dragonkie <dragonkie@hotmail.ca>
2026-03-07 15:43:41 -0500

    Reorganized system settings links

--- module/helpers/hooks.mjs
+++ module/helpers/hooks.mjs
@@ -1,47 +1,6 @@
 export default function registerHooks() {
     Hooks.once("renderSettings", async (settings, html, context, options) => {
         //==========================================================================================================
-        //> Tales from Myriad official links
-        //==========================================================================================================
-        try {
-            const section = document.createElement('section');
-            section.classList.add('flexcol');
-
-            // create the divider header
-            const divider = document.createElement('h4');
-            divider.classList.add('divider');
-            divider.textContent = 'Tales From Myriad';
-
-            // create the buttons
-            const shop = document.createElement('a');
-            shop.classList.add('button');
-            shop.href = 'https://metalweavegames.com/';
-            shop.innerHTML = `<i class="fas fa-shop"></i> Metal Weave Games`;
-
-            const book = document.createElement('a');
-            book.classList.add('button');
-            book.href = 'https://metalweavegames.com/collections/tales-from-myriad';
-            book.innerHTML = `<i class="fas fa-book"></i> Get the book!`;
-
-            // Myriad official discord server
-            const discord = document.createElement('a');
-            discord.href = 'https://discord.gg/nuq5AYgP';
-            discord.classList.add('button');
-            discord.innerHTML = `<i class="fa-brands fa-discord"></i> Discord`;
-
-            // add everything together
-            section.appendChild(divider);
-            section.appendChild(shop);
-            section.appendChild(book);
-            section.appendChild(discord);
-
-            // append it to the settings tab
-            html.appendChild(section);
-        } catch (err) {
-            console.error('Failed to append TFM browser links to settings tab');
-        }
-
-        //==========================================================================================================
         //> Developer links
         //==========================================================================================================
         try {
@@ -81,6 +40,47 @@ export default function registerHooks() {
             html.appendChild(section);
         } catch (err) {
             console.error('Failed to append developer support links');
+        }
+
+        //==========================================================================================================
+        //> Tales from Myriad official links
+        //==========================================================================================================
+        try {
+            const section = document.createElement('section');
+            section.classList.add('flexcol');
+
+            // create the divider header
+            const divider = document.createElement('h4');
+            divider.classList.add('divider');
+            divider.textContent = 'Tales From Myriad';
+
+            // create the buttons
+            const shop = document.createElement('a');
+            shop.classList.add('button');
+            shop.href = 'https://metalweavegames.com/';
+            shop.innerHTML = `<i class="fas fa-shop"></i> Metal Weave Games`;
+
+            const book = document.createElement('a');
+            book.classList.add('button');
+            book.href = 'https://metalweavegames.com/collections/tales-from-myriad';
+            book.innerHTML = `<i class="fas fa-book"></i> Get the book!`;
+
+            // Myriad official discord server
+            const discord = document.createElement('a');
+            discord.href = 'https://discord.gg/nuq5AYgP';
+            discord.classList.add('button');
+            discord.innerHTML = `<i class="fa-brands fa-discord"></i> Discord`;
+
+            // add everything together
+            section.appendChild(divider);
+            section.appendChild(shop);
+            section.appendChild(book);
+            section.appendChild(discord);
+
+            // append it to the settings tab
+            html.appendChild(section);
+        } catch (err) {
+            console.error('Failed to append TFM browser links to settings tab');
         }
     });
 }
